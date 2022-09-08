a6fea0e2231eb098b2581db0fbf3a66e8d13da30
DaveRoRa <ftdrodriguezr@gmail.com>
2022-09-08 13:40:26 -0400

    Different test formats

--- src/__tests__/greet.tsx
+++ src/__tests__/greet.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import { Greet } from "./greet";
+import { Greet } from "../components/greet/greet";
 
-it("Greet render correctly", () => {
+test("Greet render correctly", () => {
   render(<Greet />);
   const textElement = screen.getByText(/hello/i);
   expect(textElement).toBeInTheDocument();
--- src/components/greet/greet.spec.tsx
+++ src/components/greet/greet.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-it("Greet render correctly", () => {
+test("Greet render correctly", () => {
   render(<Greet />);
   const textElement = screen.getByText(/hello/i);
   expect(textElement).toBeInTheDocument();
